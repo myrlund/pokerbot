7142c6071a04f1e4990127a6e97e861837488870
John Arne Øye <johnarneoye@gmail.com>
2012-08-30 10:12:44 +0200

    blah

--- game.py
+++ game.py
@@ -7,21 +7,30 @@ class Dealer:
     def __init__(self):
         self.deck = []
         
+        
     def gen_shuffled_deck(self):
         sorted_deck = []
         for s in SUITS:
             for i in range(2, 15):
                 sorted_deck.append(Card(s,i))
         #shuffleduffle
-        deck = []
+        self.deck = []
         for i in range(0, 52):
             index = random.randint(0, 51-i)
-            deck[i] = sorted_deck[index]
+            self.deck[i] = sorted_deck[index]
             sorted_deck[index:index+1] = []
         
             
-    #deals x card(s) of it's deck
+    #deals nr_of_cards card(s) of it's deck
     def deal(self, nr_of_cards):
+        cards = []
+        for i in range(0, nr_of_cards):
+            if self.deck:
+                cards.append(self.deck.pop(0))
+            else:
+                print "Deck is empty. Shuffling new..."
+                self.gen_shuffled_deck()
+        return cards
         
         
         
@@ -37,7 +46,15 @@ class Game:
         
         
     def play(self):
-        #sumethin n stuff
+        #plain da game in turns, turns should hold for input? or maybe just gogogoo
+        self.print_gamestate()
+        
+    def print_gamestate(self):
+        for player in self.players:
+            print str(player)
+        print "- On table: "+ str(self.cards_on_table) +" $"+ str(self.pot) +" "
+        print "In deck: "+ str(self.dealer.deck)
+        
     
 
         
--- player.py
+++ player.py
@@ -1,7 +1,19 @@
 class Player:
+    #maybe we should distinguish between "intelligent" player and bots?
+    
+    #does a player need to have the distinctino betweeen the cards on the table and the hole cards?
+    #a Hand is the best hand, no distinction?
     
 
-    def __init__(self, playerid, hand):
+    def __init__(self, playerid, hand, playerpot, game):
         self.playerid = playerid #unique ID number, needed for opponent evaluation?
-        self.hand = hand
+        self.hand = hand #the Hand the player has,
+        self.playerpot = playerpot
+        self.game = game
         
+    def __str__(self):
+        return "[Player: "+ self.playerid +" Pot: $"+ str(self.playerpot) +" Cards: "+ str(self.hand) +"]"
+    
+    def pick_action(self):
+        #do stuff
+        #raise / call / fold
